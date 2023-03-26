88a47a8e8ccca12c049cd478fc232327b43bcbdb
Denisio Rordrigues <denisio@ymail.com>
2023-03-25 23:31:50 -0300

    Create and list tasks

--- src/server.js
+++ src/server.js
@@ -1,8 +1,32 @@
 import http from "node:http";
 
+const tasks = [];
+
 const server = http.createServer((req, res) => {
-  return res.end("Hello world");
-  console.log("Legal");
+  const { method, url } = req;
+
+  if (method === "GET" && url === "/tasks") {
+    res
+      .setHeader("Content-Type", "application/json")
+      .end(JSON.stringify(tasks));
+  }
+
+  if (method === "POST" && url === "/tasks") {
+    const task = {
+      Id: 1,
+      name: "Denisio",
+      email: "denisio@example.com",
+    };
+
+    tasks.push(task);
+
+    res
+      .setHeader("Conttnt-type", "application/json")
+      .writeHead(201)
+      .end(JSON.stringify(task));
+  }
+
+  res.writeHead(404).end();
 });
 
 server.listen(5000);

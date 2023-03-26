0525a348e4120a12833dd5ad74c21de2990a61cb
Denisio Rordrigues <denisio@ymail.com>
2023-03-26 01:19:51 -0300

    Separete path url with REGEX

--- src/server.js
+++ src/server.js
@@ -8,11 +8,17 @@ const server = http.createServer(async (req, res) => {
 
   const { method, url } = req;
 
+  console.log(method, url);
+
   const route = routes.find((route) => {
-    return route.method === method && route.path === url;
+    return route.method === method && route.path.test(url);
   });
 
   if (route) {
+    const routeParams = req.url.match(route.path);
+
+    console.log(routeParams);
+
     return route.handler(req, res);
   }
 
--- src/utils/build-routes-path.js
+++ src/utils/build-routes-path.js
@@ -1,5 +1,11 @@
 export function buildRoutesPath(path) {
   const routeParametersregex = /:([a-zA-Z]+)/g;
+  const pathWithParams = path.replaceAll(
+    routeParametersregex,
+    "(?<$1>[a-z0-9\\-_]+)"
+  );
+  const pathRegex = new RegExp(`^${pathWithParams}`);
 
-  console.log(Array.from(path.matchAll(routeParametersregex)));
+  return pathRegex;
+  //   console.log(Array.from(path.matchAll(routeParametersregex)));
 }
